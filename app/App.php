94a20b5e38b5611b23573fc201543533ac73ea68
alex_nemynov <alexnemynov@yandex.ru>
2024-09-06 21:12:33 +0300

    added fn getTransactionsFiles, fn getTransactions

--- app/App.php
+++ app/App.php
@@ -1,1 +1,35 @@
-s<?php
+<?php
+
+declare(strict_types=1);
+
+// Your code
+function getTransactionFiles(string $dirPath): array
+{
+    $files = [];
+
+    foreach (scandir($dirPath) as $file) {
+        if (is_dir($file)) {
+            continue;
+        } else {
+            $files[] = $dirPath . $file;
+        }
+    }
+    return $files;
+}
+
+function getTransactions(string $fileName)
+{
+    if (! file_exists($fileName)) {
+        trigger_error('File "' . $fileName . '" does not exist', E_USER_ERROR);
+    }
+
+    $file = fopen($fileName, 'r');
+    $transactions = [];
+
+    fgetcsv($file);
+    while (($transaction = fgetcsv($file)) !== false) {
+        $transactions[] = $transaction;
+    }
+    fclose($file);
+    return $transactions;
+}
